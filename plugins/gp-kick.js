d7315ecf90c7a5841a3bb246c5c42ec70c050487
Jangl2004 <alessandromagno722@gmail.com>
2026-03-08 00:14:11 +0100

    Update gp-kick.js
    Signed-off-by: Jangl2004 <alessandromagno722@gmail.com>

--- plugins/gp-kick.js
+++ plugins/gp-kick.js
@@ -1,7 +1,4 @@
 async function handler(m, { isBotAdmin, isOwner, text, conn }) {
-  const body = (m.text || '').toLowerCase().trim()
-    if (!body.match(/^(kick|ban|kickdaanna|avadachedavra|sparisci|pannolini|puffo)(\s|$)/i)) return
-  
   if (!isBotAdmin) {
     return await conn.sendMessage(m.chat, {
       text: 'ⓘ Devo essere admin per poter funzionare'
@@ -64,7 +61,7 @@ async function handler(m, { isBotAdmin, isOwner, text, conn }) {
   await conn.groupParticipantsUpdate(m.chat, [mention], 'remove')
 }
 
-handler.customPrefix = /kick|cicciona|ciabbon|ban|kickdaanna|avadachedavra|pannolini|puffo/i
+handler.customPrefix = /kick|avadachedavra|kickdaanna|ciabbon|cicciona|pannolini|puffo/i
 handler.command = new RegExp
 handler.admin = true
 
